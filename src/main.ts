a085ff10db2386d2c5c8ef73d6ae4f06fc06653c
Yusuke Kawamoto <novogrammer@gmail.com>
2026-09-14 03:39:24 +0900

    ADSRで変化させる

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,78 @@
-import { setupSpessasynthAsync } from './spessasynth';
+import { createPlayerAsync } from './player';
 import './style.scss'
 import * as THREE from "three";
 
-import gsap from "gsap";
+// import gsap from "gsap";
 
+type Adsr = {
+  attack: number;
+  decay: number;
+  sustain: number;
+  release: number;
+};
+
+
+interface NoteState {
+  midiNote: number,
+  startedAt: number | null;
+  releasedAt: number | null;
+  velocity: number;
+}
+
+function getAdsrValue(
+  now: number,
+  noteState: NoteState,
+  adsr: Adsr,
+): number {
+  if(noteState.startedAt === null){
+    return 0;
+  }
+  const attackEnd = noteState.startedAt + adsr.attack;
+  const decayEnd = attackEnd + adsr.decay;
+
+  if (noteState.releasedAt === null || now < noteState.releasedAt) {
+    if (now < attackEnd) {
+      return (now - noteState.startedAt) / adsr.attack;
+    }
+
+    if (now < decayEnd) {
+      const t = (now - attackEnd) / adsr.decay;
+      return 1 + (adsr.sustain - 1) * t;
+    }
+
+    return adsr.sustain;
+  }
+
+  // releaseされてなかった場合の値
+  const releaseStartValue = getAdsrValue(
+    noteState.releasedAt,
+    {
+      ...noteState,
+      releasedAt: null,
+    },
+    adsr,
+  );
+
+  const t = (now - noteState.releasedAt) / adsr.release;
+
+  return Math.max(0, releaseStartValue * (1 - t));
+}
+
+function calcMatrix(objectDummy:THREE.Object3D,now:number,noteState:NoteState){
+  const adsr: Adsr = {
+    attack: 0.08,
+    decay: 0.15,
+    sustain: 0.6,
+    release: 0.3,
+  };
+  const envelope = getAdsrValue(now,noteState,adsr);
+
+  const scale=envelope * noteState.velocity / 127 + 0.05;
+  objectDummy.scale.setScalar(scale);
+  objectDummy.position.x=THREE.MathUtils.mapLinear(noteState.midiNote,0,127,-5,5);
+  objectDummy.updateMatrix();
+
+}
 
 async function mainAsync(){
 
@@ -21,12 +90,20 @@ async function mainAsync(){
   const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
   const cube = new THREE.InstancedMesh( geometry, material, 128 );
   cube.instanceMatrix.setUsage( THREE.DynamicDrawUsage );
+  const noteStateList:NoteState[]=[];
+  
+
   const objectDummy=new THREE.Object3D();
   for(let i=0;i<128;i++){
-    objectDummy.scale.setScalar(0.05);
-    objectDummy.position.x=THREE.MathUtils.mapLinear(i,0,127,-5,5);
-    objectDummy.updateMatrix();
+    const noteState:NoteState={
+      midiNote:i,
+      startedAt:null,
+      releasedAt:null,
+      velocity:0,
+    }
+    calcMatrix(objectDummy,0,noteState);
     cube.setMatrixAt(i,objectDummy.matrix);
+    noteStateList.push(noteState);
   }
   scene.add( cube );
 
@@ -37,36 +114,44 @@ async function mainAsync(){
   // }
 
 
+  const player=await createPlayerAsync({
+    onNoteOn:(event)=>{
+      console.log("noteOn",event);
+      const noteState=noteStateList[event.midiNote];
+      if(!noteState){
+        throw new Error("noteState is null");
+      }
+      noteState.startedAt=player.seq.currentHighResolutionTime;
+      noteState.releasedAt=null;
+      noteState.velocity=event.velocity;
+    },
+    onNoteOff:(event)=>{
+      console.log("noteOff",event);
 
-  renderer.setAnimationLoop((time)=>{
-    // cube.rotation.x = time / 2000;
-    // cube.rotation.y = time / 1000;
+      const noteState=noteStateList[event.midiNote];
+      if(!noteState){
+        throw new Error("noteState is null");
+      }
+      noteState.releasedAt=player.seq.currentHighResolutionTime;
+    },
+  });
+
+  renderer.setAnimationLoop(()=>{
+    for(let i=0;i<128;i++){
+      const noteState=noteStateList[i];
+      if(!noteState){
+        throw new Error("noteState is null");
+      }
+      calcMatrix(objectDummy,player.seq.currentHighResolutionTime,noteState);
+      cube.setMatrixAt(i,objectDummy.matrix);
+    }
+    cube.instanceMatrix.needsUpdate=true;
 
     renderer.render( scene, camera );
 
   })
 
 
-  await setupSpessasynthAsync({
-    onNoteOn:(event)=>{
-      console.log("noteOn",event);
-      const index=THREE.MathUtils.clamp(event.midiNote,0,127);
-      objectDummy.position.x=THREE.MathUtils.mapLinear(index,0,127,-5,5);
-      objectDummy.scale.setScalar(0.1);
-      objectDummy.updateMatrix();
-      cube.setMatrixAt(index,objectDummy.matrix);
-      cube.instanceMatrix.needsUpdate=true;
-    },
-    onNoteOff:(event)=>{
-      console.log("noteOff",event);
-      const index=THREE.MathUtils.clamp(event.midiNote,0,127);
-      objectDummy.position.x=THREE.MathUtils.mapLinear(index,0,127,-5,5);
-      objectDummy.scale.setScalar(0.05);
-      objectDummy.updateMatrix();
-      cube.setMatrixAt(index,objectDummy.matrix);
-      cube.instanceMatrix.needsUpdate=true;
-    },
-  });
 }
 
 
---
+++ src/player.ts
@@ -0,0 +1,99 @@
+import { Sequencer, WorkletSynthesizer } from 'spessasynth_lib';
+
+import workletUrl from "spessasynth_lib/dist/spessasynth_processor.min.js?url";
+import { onHmrDispose } from './hmr_utils';
+
+
+async function loadAsArrayBufferAsync(url:string){
+  const response = await fetch(url);
+  return await response.arrayBuffer();
+}
+
+
+
+type NoteOnEvent = {
+  midiNote: number;
+  channel: number;
+  velocity: number;
+};
+type NoteOffEvent = {
+  midiNote: number;
+  channel: number;
+};
+
+const EVENT_ID_VISUALIZER="visualizer";
+
+
+class Player{
+  resumeElement = document.querySelector<HTMLElement>("#resume")!;
+  currentTimeElement = document.querySelector<HTMLElement>("[data-role='current-time'")!;
+  durationElement = document.querySelector<HTMLElement>("[data-role='duration'")!;
+
+  audioContext:AudioContext;
+  synth:WorkletSynthesizer;
+  seq:Sequencer;
+  onNoteOn:(event:NoteOnEvent)=>void;
+  onNoteOff:(event:NoteOffEvent)=>void;
+
+  constructor(audioContext: AudioContext,synth: WorkletSynthesizer,seq: Sequencer,onNoteOn:(event:NoteOnEvent)=>void,onNoteOff:(event:NoteOffEvent)=>void){
+    this.audioContext=audioContext;
+    this.synth=synth;
+    this.seq=seq;
+    this.onNoteOn=onNoteOn;
+    this.onNoteOff=onNoteOff;
+    this.setupEvents();
+  }
+  setupEvents(){
+
+    this.synth.eventHandler.addEvent("noteOn",EVENT_ID_VISUALIZER,this.onNoteOn);
+    this.synth.eventHandler.addEvent("noteOff",EVENT_ID_VISUALIZER,this.onNoteOff);
+    onHmrDispose(()=>{
+      this.synth.eventHandler.removeEvent("noteOn",EVENT_ID_VISUALIZER);
+      this.synth.eventHandler.removeEvent("noteOff",EVENT_ID_VISUALIZER);
+    });
+
+
+    const intervalTimer=setInterval(()=>{
+      this.currentTimeElement.textContent=this.seq.currentTime.toFixed(2);
+      this.durationElement.textContent=this.seq.duration.toFixed(2);
+    },100);
+    onHmrDispose(()=>{
+      clearInterval(intervalTimer);
+    });
+
+    this.resumeElement.addEventListener("click",()=>{
+      (async ()=>{
+        
+        if(this.seq.paused){
+          await this.audioContext.resume();
+          this.seq.play();
+          this.resumeElement.textContent="Pause";
+        }else{
+          this.seq.pause();
+          this.resumeElement.textContent="Resume";
+        }
+      })().catch((error)=>console.error(error));
+    })
+
+  }
+}
+
+
+export async function createPlayerAsync({onNoteOn,onNoteOff}:{onNoteOn:(event:NoteOnEvent)=>void,onNoteOff:(event:NoteOffEvent)=>void}):Promise<Player>{
+  const sfFile = await loadAsArrayBufferAsync("./assets/soundfonts/GeneralUser-GS/GeneralUserGS.sf3");
+
+  const audioContext = new AudioContext();
+  await audioContext.audioWorklet.addModule(workletUrl);
+  const synth = new WorkletSynthesizer(audioContext);
+  synth.connect(audioContext.destination);
+  await synth.soundBankManager.addSoundBank(sfFile, "main");
+  const seq = new Sequencer(synth);
+  seq.loopCount = Infinity;
+  const midiFile = await loadAsArrayBufferAsync("./assets/smf/fur_Elise_WoO59.mid");
+  seq.loadNewSongList([{ binary: midiFile }]);
+
+  const player = new Player(audioContext,synth,seq,onNoteOn,onNoteOff);
+
+  return player;
+}
+
